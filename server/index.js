f1ae0147d22db5cd916c4c985aad98adf47fc18a
dalva19 <75229602+dalva19@users.noreply.github.com>
2021-07-31 15:19:37 -0400

    package.json moved to server folder

--- server/index.js
+++ server/index.js
@@ -1,4 +1,10 @@
 const express = require("express");
 const app = express();
 
+//import routes
+const authRoute = require("./routes/auth");
+
+//route middleware
+app.use("/co-health/user", authRoute);
+
 app.listen(3000, () => console.log("Server is running on port 3000."));
