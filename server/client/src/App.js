c4a35033d0f43acde86fa46336466cc0f472f8e5
dalva19 <75229602+dalva19@users.noreply.github.com>
2021-07-30 21:54:57 -0400

    architecture folders and react router

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import "./App.css";
+//components
+import Home from "./components/Home";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/home" />
+          </Route>
+          <Route exact path="/home" component={Home} />
+        </Switch>
+      </Router>
+    </>
   );
-}
+};
 
 export default App;
---
+++ server/client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Co-Health</h1>
+      <h3>Helping mind the gap in community healthcare.</h3>
+    </div>
+  );
+};
+
+export default Home;
