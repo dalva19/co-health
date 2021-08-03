4323bbccd472d87e77a1722896b224306437a6d3
dalva19 <75229602+dalva19@users.noreply.github.com>
2021-08-03 11:45:47 -0400

    GET /requests

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const Request = require("../models/Request");
 const User = require("../models/User");
 
+//routes on co-health/profile
+
 router.get("/", async (req, res) => {
   res.status(200);
 
@@ -28,34 +30,34 @@ router.get("/", async (req, res) => {
   }
 });
 
-//figure out how to populate username into params from login
-router.get("/:username", async (req, res) => {
-  if (req.user.username === req.params.username) {
-    res.status(200);
-    //get profile info from db
-    try {
-      await User.findById({ _id: req.user._id })
-        .populate("requests")
-        .exec((err, user) => {
-          if (err) {
-            console.log(err);
-          } else {
-            res.send({
-              name: user.name,
-              username: user.username,
-              profileType: user.profileType,
-              address: user.address,
-              credentials: user.credentials || null,
-              requests: user.requests || null,
-              offers: user.offers || null,
-            });
-          }
-        });
-    } catch (err) {
-      res.status(400).send(err);
-    }
-  }
-});
+// //figure out how to populate username into params from login
+// router.get("/:username", async (req, res) => {
+//   if (req.user.username === req.params.username) {
+//     res.status(200);
+//     //get profile info from db
+//     try {
+//       await User.findById({ _id: req.user._id })
+//         .populate("requests")
+//         .exec((err, user) => {
+//           if (err) {
+//             console.log(err);
+//           } else {
+//             res.send({
+//               name: user.name,
+//               username: user.username,
+//               profileType: user.profileType,
+//               address: user.address,
+//               credentials: user.credentials || null,
+//               requests: user.requests || null,
+//               offers: user.offers || null,
+//             });
+//           }
+//         });
+//     } catch (err) {
+//       res.status(400).send(err);
+//     }
+//   }
+// });
 
 router.post("/request", async (req, res) => {
   //need to add validation
@@ -81,7 +83,31 @@ router.post("/request", async (req, res) => {
   }
 });
 
+router.get("/requests", async (req, res) => {
+  res.status(200);
+
+  try {
+    await User.findById({ _id: req.user._id })
+      .populate("requests")
+      .exec((err, user) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send({
+            requests: user.requests || null,
+          });
+        }
+      });
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
 //put '/request'
 //delete '/request'
+// router.delete('/requests/:request', async (req, res) => {
+//   const update = {$pull: {requests: req._id}}
+
+// })
 
 module.exports = router;
