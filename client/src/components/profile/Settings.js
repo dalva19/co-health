743d0c92c1ebfcc840f2a06dba507fc7d3889059
dalva19 <75229602+dalva19@users.noreply.github.com>
2021-08-29 13:33:16 -0400

    tables in settings component

--- client/src/components/profile/Settings.js
+++ client/src/components/profile/Settings.js
@@ -3,20 +3,73 @@ import { useState } from "react";
 import NavigationTabs from "./NavigationTabs";
 import SettingsForm from "./SettingsForm";
 //styling
-import { Button } from "react-bootstrap";
+import { Button, Row, Col, Container, Table } from "react-bootstrap";
 
 const Settings = () => {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  //loads with profile info based on who is logged in
   return (
     <>
       <NavigationTabs defaultActiveKey="/co-health/profile/settings" />
-      <h2>this is the settings page</h2>
 
-      <Button onClick={handleShow}>Manage Settings</Button>
+      <Container>
+        <Row>
+          <h2>Settings</h2>
+          <Col>
+            <Table bordered hover>
+              <thead>
+                <tr>
+                  <th colSpan="2">Personal Information</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>First Name</td>
+                  <td>NAME</td>
+                </tr>
+                <tr>
+                  <td>Last Name</td>
+                  <td>Last NAME</td>
+                </tr>
+                <tr>
+                  <td>Community</td>
+                  <td>Durham</td>
+                </tr>
+              </tbody>
+            </Table>
+          </Col>
+          <Col>
+            <Table bordered hover>
+              <thead>
+                <tr>
+                  <th colSpan="2">Address</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Street</td>
+                  <td>114 Chestnut Ct</td>
+                </tr>
+                <tr>
+                  <td>City</td>
+                  <td>Durham</td>
+                </tr>
+                <tr>
+                  <td>State</td>
+                  <td>NC</td>
+                </tr>
+                <tr>
+                  <td>Zipcode</td>
+                  <td>27712</td>
+                </tr>
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+        <Button onClick={handleShow}>Manage Settings</Button>
+      </Container>
 
       <SettingsForm
         show={show}
