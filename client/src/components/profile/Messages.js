3cc48b32880c867d01aade2722a8f82b7714edb9
dalva19 <75229602+dalva19@users.noreply.github.com>
2021-09-05 10:53:59 -0400

    basic socket messenger working

--- client/src/components/profile/Messages.js
+++ client/src/components/profile/Messages.js
@@ -1,13 +1,82 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import io from "socket.io-client";
+import { InputGroup, FormControl } from "react-bootstrap";
 //components
 import NavigationTabs from "./NavigationTabs";
 
+const socket = io.connect("http://localhost:8000");
+
 const Messages = () => {
+  const [state, setState] = useState({ message: "", name: "" });
+  const [chat, setChat] = useState([]);
+
+  useEffect(() => {
+    socket.on("message", ({ name, message }) => {
+      setChat([...chat, { name, message }]);
+    });
+  });
+
+  //helper fxns
+  const onTextChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+
+  const onMessageSubmit = (e) => {
+    e.preventDefault();
+    const { name, message } = state;
+    socket.emit("message", { name, message });
+    setState({ message: "", name });
+  };
+
+  const renderChat = () => {
+    return chat.map(({ name, message }, index) => (
+      <div key={index}>
+        <h3>
+          {name}: <span>{message}</span>
+        </h3>
+      </div>
+    ));
+  };
+
   //loads with profile info based on who is logged in
   return (
     <>
       <NavigationTabs defaultActiveKey="/co-health/profile/messages" />
-      <h2>this is the messages page</h2>
+      <div>
+        <form onSubmit={onMessageSubmit}>
+          <h1>Messenger</h1>
+          <div className="name-field">
+            <InputGroup className="mb-3">
+              <InputGroup.Text>Name</InputGroup.Text>
+              <FormControl
+                aria-label="Name"
+                name="name"
+                onChange={(e) => onTextChange(e)}
+                value={state.name}
+              />
+            </InputGroup>
+          </div>
+
+          <div>
+            <InputGroup className="mb-3">
+              <InputGroup.Text id="basic-addon1">Message</InputGroup.Text>
+              <FormControl
+                aria-label="Message"
+                name="message"
+                onChange={(e) => onTextChange(e)}
+                value={state.message}
+              />
+            </InputGroup>
+          </div>
+
+          <button>Send</button>
+        </form>
+
+        <div className="render-chat">
+          <h1>Chat Log</h1>
+          {renderChat()}
+        </div>
+      </div>
     </>
   );
 };
